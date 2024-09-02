b844ed669d1155dfdf7ae753ffbaec19a90b6a0d
IOSH <iveninvala7@gmal.com>
2024-09-02 13:12:29 +0300

    Add initializer Grid

--- game-era-war-and-tactics/src/main.rs
+++ game-era-war-and-tactics/src/main.rs
@@ -1,6 +1,7 @@
+use core_domain::{grid::Grid, settings::Settings, types::Dim};
 use macroquad::prelude::*;
 
-#[macroquad::main("BasicShapes")]
+#[macroquad::main("Эпоха Войны и Тактики")]
 async fn main() {
     loop {
         clear_background(RED);
@@ -9,6 +10,12 @@ async fn main() {
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
         draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
         draw_text("HELLO", 20.0, 20.0, 20.0, DARKGRAY);
+
+        let settings = Settings {
+            grid_size: Dim::new(4, 4),
+        };
+        let grid = Grid::new(settings.grid_size);
+        println!("{}", grid);
 
         next_frame().await
     }
